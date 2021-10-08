3b461ee891187b57e078b87a6d115b4101ee065d
Ayush Pattnaik <ayp.scp@gmail.com>
2021-10-08 21:32:55 +0530

    new codes were added.

---
+++ codeStreak/58. Length of Last Word/src/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int len = 0;
+        s = s.trim();
+        for (int i = s.length() - 1; i >=0; i --){
+            if (s.charAt(i) == ' '){
+                break;
+            }
+            else{
+                len ++;
+            }
+        }
+        return len;
+    }
+}
