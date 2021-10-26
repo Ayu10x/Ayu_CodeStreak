21aa3317a8e82fc3b213340a9575562e022defde
Ayush Pattnaik <ayp.scp@gmail.com>
2021-10-26 22:05:29 +0530

    New Solutions are added.

---
+++ codeStreak/326. Power of Three/src/Solution.java
@@ -0,0 +1,42 @@
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n <= 0){
+            return false;
+        }
+        while(n > 1){
+            if(n % 3 !=0){
+                return false;
+            }
+            n = n / 3;
+        }
+        return true;
+    }
+}
+/*
+Solution2:
+
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        return n>0?(1162261467 % n) == 0:false;
+    }
+}
+
+Solution 3:
+
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+         return (n == 1 || n == 3 || n == 9 || n == 27 || n == 81 || n == 243 || n == 729 || n == 2187 || n == 6561 || n == 19683 || n == 59049 || n == 177147 || n == 531441 || n == 1594323 || n == 4782969 || n == 14348907 || n == 43046721 || n == 129140163 || n == 387420489 || n == 1162261467);
+    }
+}
+
+Solution 4:
+
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        double res = Math.log10(n) / Math.log10(3);
+        return Double.compare(res, (int)res) == 0;
+    }
+}
+
+
+ */
